0a7a225f1493d7ea08027048ac668489446c9919
Garid999 <garidga999@gmail.com>
2026-02-28 12:50:47 +0800

    2.26 class changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/widgets_layout_demo.dart';
-import 'dart_demo_screen.dart'; // Import the new screen file
+import 'package:flutter_project/ui/2_widget_layout/widgets_layout_demo.dart';
+import 'package:flutter_project/ui/state_management/3_state_management_demo.dart';
+import 'ui/1_dart/dart_demo_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: const HomeScreen(),
     );
   }
@@ -26,27 +27,45 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: const Text("Home Screen"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
+      // A ListView for your items
       body: ListView(
         children: [
           ListTile(
-            title: const Text('1. Dart Demo'),
-            trailing: const Icon(Icons.arrow_forward_ios),
+            title: const Text("1. Dart Demo"),
+            leading: const Icon(Icons.code),
             onTap: () {
-              // Navigate to the second screen
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const DartDemoScreen()),
               );
             },
-          ),ListTile(
-            title: const Text('2. Widgets and layout'),
-            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+
+          ListTile(
+            title: const Text("2. Widgets and layout"),
+            leading: const Icon(Icons.code),
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const WidgetsLayoutDemo()),
+                MaterialPageRoute(
+                  builder: (context) => const WidgetsLayoutDemo(),
+                ),
+              );
+            },
+          ),
+
+          ListTile(
+            title: const Text("3. State management"),
+            leading: const Icon(Icons.code),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const StateManagementDemo(),
+                ),
               );
             },
           ),
---
+++ lib/ui/state_management/3_state_management_demo.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class StateManagementDemo extends StatefulWidget {
+  const StateManagementDemo({super.key});
+
+  @override
+  State<StateManagementDemo> createState() => _StateManagementDemoState();
+}
+
+class _StateManagementDemoState extends State<StateManagementDemo> {
+  final colors = [
+    Colors.amber,
+    Colors.black,
+    Colors.red,
+    Colors.green,
+    Colors.cyan,
+    Colors.purple,
+    Colors.orange,
+    Colors.white,
+  ];
+  int colorIndex = 0;
+  final colorIndexNotifier = ValueNotifier(0);
+
+  int number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('State Management')),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Container(color: colors[colorIndex], width: 200, height: 200,
+            child: Center(
+              child: Text("$number", style: TextStyle(fontSize: 50),),
+            ),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(onPressed: changeColor, child: Text('Change color')),
+            const SizedBox(height: 20),
+            OutlinedButton(onPressed: changeText, child: Text('Change text')),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void changeColor() {
+      setState(() {
+    colorIndex++;
+    colorIndex = colorIndex % colors.length;
+    print(colorIndex);
+    });
+  }
+
+  void changeText() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+}
